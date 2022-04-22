ea6ec2574e81df6a9bcfeaa57fba5e9092a14406
Argo-Lenovo <argo@163.com>
2022-04-23 01:29:58 +0800

    doc: 增加示例说明与资源文件

--- src/BootstrapBlazor.Shared/Samples/Reconnectors.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Reconnectors.razor.cs
@@ -2,37 +2,41 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using BootstrapBlazor.Components;
 using BootstrapBlazor.Shared.Common;
 
 namespace BootstrapBlazor.Shared.Samples;
 
+/// <summary>
+/// Reconnectors 示例
+/// </summary>
 public partial class Reconnectors
 {
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
         new AttributeItem()
         {
-            Name = "ReconnectingTemplate",
-            Description = Localizer["Event4"],
+            Name = nameof(Reconnector.ReconnectingTemplate),
+            Description = Localizer["ReconnectingTemplateAttr"],
             Type = "RenderFragment",
-            ValueList = "",
-            DefaultValue = ""
+            ValueList = " — ",
+            DefaultValue = " — "
         },
         new AttributeItem()
         {
-            Name = "ReconnectFailedTemplate",
-            Description = Localizer["Event5"],
+            Name = nameof(Reconnector.ReconnectFailedTemplate),
+            Description = Localizer["ReconnectFailedTemplateAttr"],
             Type = "RenderFragment",
-            ValueList = "",
-            DefaultValue = ""
+            ValueList = " — ",
+            DefaultValue = " — "
         },
         new AttributeItem()
         {
-            Name = "ReconnectRejectedTemplate",
-            Description = Localizer["Event6"],
+            Name = nameof(Reconnector.ReconnectRejectedTemplate),
+            Description = Localizer["ReconnectRejectedTemplateAttr"],
             Type = "RenderFragment",
-            ValueList = "",
-            DefaultValue = ""
+            ValueList = " — ",
+            DefaultValue = " — "
         },
     };
 }
