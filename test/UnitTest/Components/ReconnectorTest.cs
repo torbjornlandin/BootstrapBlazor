578ca62b98a7abaa81590482df5d0a6123186fd4
Argo-Lenovo <argo@163.com>
2022-04-24 10:02:19 +0800

    test: 增加 outlet 单元测试

---
+++ test/UnitTest/Components/ReconnectorTest.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components.Web;
+
+namespace UnitTest.Components;
+
+public class ReconnectorTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void ReconnectorOutlet_Ok()
+    {
+        var cut = Context.RenderComponent<ReconnectorOutlet>();
+        cut.Contains("components-reconnect-modal");
+    }
+}
